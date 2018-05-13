3f593ab5bc0048be4012c4655231ffa8d3baff65
HANECA <hane.ca.info@gmail.com>
2018-05-13 09:08:29 -0700

    add cors configuration to the specific url

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var router = express.Router();
+// 新たに追加
+var cors = require('cors');
+var corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200,
+};
 
-router.get('/api/sample', function(req, res, next) {
+// corsの処理を追加
+router.get('/api/sample', cors(corsOptions), function(req, res, next) {
   res.json('success');
 });
 
